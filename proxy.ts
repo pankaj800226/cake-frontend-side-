5da0bc2fd42216eaa1e83c7f5e49c7dd35b9f00c
Pankaj kumar <pankajsingh@Mac.lan>
2026-07-03 13:58:46 +0530

    new code added

---
+++ proxy.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(req: NextRequest) {
+  const token = req.cookies.get("adminToken");
+
+  if (!token?.value) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/allProduct",
+    "/cakes",
+    "/AllCakes",
+    "/CategoryFilter",
+  ],
+};
