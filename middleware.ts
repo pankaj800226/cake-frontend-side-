801bc78820f892f5b02e10b519dacec91bcfaa8c
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 14:47:34 +0530

    new code added

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,9 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("userToken");
 
+  console.log("Token:", token);
+
+
   console.log(req.cookies.getAll());
   
 
