a35a902a8f274fc56a715ee51118b34b8912e052
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 14:59:51 +0530

    new code added

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,34 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("userToken");
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("userToken");
 
-  console.log("Token:", token);
+//   console.log("Token:", token);
 
 
-  console.log(req.cookies.getAll());
+//   console.log(req.cookies.getAll());
   
 
-  if (!token?.value) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   if (!token?.value) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     "/",
+//     "/allProduct",
+//     "/cakes",
+//     "/AllCakes",
+//     "/CategoryFilter",
+//   ],
+// };
+
+import { NextResponse } from "next/server";
+
+export function middleware() {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/",
-    "/allProduct",
-    "/cakes",
-    "/AllCakes",
-    "/CategoryFilter",
-  ],
-};
