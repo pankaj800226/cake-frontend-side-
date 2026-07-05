4b7d8f1362017eb89b068d8d7dad8c48a85ba3a6
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 20:59:20 +0530

    new code added

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,11 @@ import type { NextRequest } from "next/server";
 
 function proxy(req: NextRequest) {
   const token = req.cookies.get("userToken");
+  const path = req.nextUrl.pathname;
 
 
-  if (!token?.value) {
+
+  if (!token?.value && path.startsWith('/cakes')) {
     return NextResponse.redirect(
         new URL('/login', req.url)
     )
