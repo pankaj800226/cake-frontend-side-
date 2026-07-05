f13ee9a7d901b8202f3b4496fdc65101ed3a18d8
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 17:55:27 +0530

    new code added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -61,8 +61,7 @@ const Login = () => {
                 localStorage.setItem("phone", res.data.phone);
 
                 toast.success("Welcome back!");
-                // router.push('/');
-                window.location.href = "/";
+                router.push('/');
 
             }
 
