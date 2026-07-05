9e152fabd01554a1a5801dd817f171f210bc96c9
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 21:14:17 +0530

    new code added

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,3 @@
-// 📁 middleware.ts
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -7,30 +5,20 @@ function proxy(req: NextRequest) {
   const token = req.cookies.get("userToken");
   const path = req.nextUrl.pathname;
 
+  if (!token?.value && path.startsWith('/cakes')) {
+    return NextResponse.redirect(
+        new URL('/login', req.url)
+    )
+}
 
-  // ✅ 1. Login page - ALWAYS allow (infinite loop se bachne ke liye)
-  if (path === "/login") {
-    console.log("🟢 Login page - ALLOWING");
-    return NextResponse.next();
-  }
-
-  // ✅ 2. Sirf /cakes routes check karo
-  if (path.startsWith("/cakes")) {
-    if (!token?.value) {
-      console.log("🔴 No token - Redirecting to /login");
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-    console.log("🟢 Token valid - Allowing access");
-  }
-
-  // ✅ 3. Baaki sab routes (home, etc.) - ALLOW
   return NextResponse.next();
 }
 
-export default proxy;
+export default proxy; 
 
 export const config = {
   matcher: [
-    "/cakes/:path*",  // ✅ Sirf cakes catch karo
+    // "/",
+    "/cakes/:path*",
   ],
 };
