de59a42089557bf6fb5a6fb43d728b22463bfff3
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-05 20:55:11 +0530

    new code added

--- proxy.ts
+++ proxy.ts
@@ -1,33 +1,20 @@
-// 📁 middleware.ts
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 function proxy(req: NextRequest) {
   const token = req.cookies.get("userToken");
-  const path = req.nextUrl.pathname;
 
-  // ✅ Home page PUBLIC (no login required)
-  if (path === "/") {
-    console.log("🏠 Home - PUBLIC");
-    return NextResponse.next();
-  }
 
-  // ✅ Login page PUBLIC
-  if (path === "/login") {
-    return NextResponse.next();
-  }
-
-  // ✅ Protected routes - Token VALUE check
   if (!token?.value) {
-    console.log("🔴 Redirecting to /login");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+    return NextResponse.redirect(
+        new URL('/login', req.url)
+    )
+}
 
   return NextResponse.next();
 }
 
-export default proxy;
+export default proxy; 
 
 export const config = {
   matcher: [
