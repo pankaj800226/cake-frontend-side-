a48e2a8c49d1b0bb3d18599cbc2f4e91e8bb8d0e
Pankaj kumar <pankajsingh@Pankajs-MacBook-Air.local>
2026-07-15 10:43:28 +0530

    new code added

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("userToken");
+
+  if (!token?.value) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/cakes",
+  ],
+};
